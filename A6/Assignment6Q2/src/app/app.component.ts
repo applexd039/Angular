5c5e533e9dae70be465d80a552d6537363076fdf
applexd039 <105958031+applexd039@users.noreply.github.com>
2023-04-22 14:19:13 +0530

    Add files via upload

---
+++ A6/Assignment6Q2/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment6q1';
+
+  DisplayDetails()
+  {
+    return "Marvellous Infosystems"
+  }
+  
+}
